79c1cc2140d8f5e05e8d63a97b1c9df57938eee6
andy5 <andy518420@gmail.com>
2018-08-24 22:44:05 +0800

    bolin0

---
+++ app/src/main/java/com/kcl/nushhack/potassiumchloride/teacher.java
@@ -0,0 +1,12 @@
+package com.kcl.nushhack.potassiumchloride;
+
+import java.util.List;
+
+/**
+ * Created by andy5 on 24/8/2018.
+ */
+
+public class teacher extends user {
+    List<module> module;
+
+}
