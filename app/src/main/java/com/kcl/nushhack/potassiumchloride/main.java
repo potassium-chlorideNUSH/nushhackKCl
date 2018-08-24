ff55f74a1a79521807d7e3988f8eabdd5189b2f5
andy5 <andy518420@gmail.com>
2018-08-25 00:11:19 +0800

    login

--- app/src/main/java/com/kcl/nushhack/potassiumchloride/main.java
+++ app/src/main/java/com/kcl/nushhack/potassiumchloride/main.java
@@ -4,11 +4,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class main extends AppCompatActivity {
-
+    user user;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         String name=getIntent().getStringExtra("name");
+        //user=find by name in database like user=new student(blabla);
     }
 }
