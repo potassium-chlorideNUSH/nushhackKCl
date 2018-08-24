536a8471bd2fd472c23196a49a4d66b0caa5e89d
andy5 <andy518420@gmail.com>
2018-08-25 01:43:52 +0800

    login

--- app/src/main/java/com/kcl/nushhack/potassiumchloride/login.java
+++ app/src/main/java/com/kcl/nushhack/potassiumchloride/login.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -86,23 +87,21 @@ public class login extends AppCompatActivity {
 
     public void login(View view) {
         loginButton.setEnabled(false);
-        if (!check()) {
-            loginFail();
-            return;
-        }
         final ProgressDialog progressDialog = new ProgressDialog(login.this);
         progressDialog.setIndeterminate(true);
         progressDialog.setMessage("Logging in...");
         progressDialog.show();
-        String id = idText.getText().toString();
-        String password = passwordtext.getText().toString();
+        if (!check()) {
+            loginFail();
+            return;
+        }
         new android.os.Handler().postDelayed(
                 new Runnable() {
                     public void run() {
                         progressDialog.dismiss();
                         loginPass();
                     }
-                }, 1000);
+                }, 3000);
     }
 
     void loginFail(){
@@ -115,6 +114,7 @@ public class login extends AppCompatActivity {
         Intent intent=new Intent(this,main.class);
         intent.putExtra("name",idText.getText().toString());
         loginButton.setEnabled(true);
+        startActivity(intent);
     }
     public boolean check() {
         boolean valid = true;
--- app/src/main/java/com/kcl/nushhack/potassiumchloride/user.java
+++ app/src/main/java/com/kcl/nushhack/potassiumchloride/user.java
@@ -3,4 +3,15 @@ package com.kcl.nushhack.potassiumchloride;
 abstract class user {
     String name, email;
     int id;
+
+    public notification[] getNotifications() {
+        return notifications;
+    }
+
+    public void setNotifications(notification[] notifications) {
+        this.notifications = notifications;
+    }
+
+    notification[] notifications;
+
 }
