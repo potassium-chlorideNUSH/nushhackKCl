651a09d1acce0cc0d8c045013a45e9ff042344ce
Kerk Tai Heng <bionicrobojistukerk@gmail.com>
2018-08-25 01:57:14 +0800

    msg service

---
+++ app/src/main/java/com/kcl/nushhack/potassiumchloride/firebase_messaging_service.java
@@ -0,0 +1,37 @@
+package com.kcl.nushhack.potassiumchloride;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+
+import com.firebase.jobdispatcher.FirebaseJobDispatcher;
+import com.firebase.jobdispatcher.GooglePlayDriver;
+import com.firebase.jobdispatcher.Job;
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class firebase_messaging_service extends FirebaseMessagingService{
+
+    private static final String TAG = "MyFirebaseMsgService";
+
+    @Override
+    public void onNewToken(String token) {
+        Log.d(TAG, "Refreshed token: " + token);
+
+        // If you want to send messages to this application instance or
+        // manage this apps subscriptions on the server side, send the
+        // Instance ID token to your app server.
+        //sendRegistrationToServer(token);
+    }
+
+}
+
