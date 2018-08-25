b54a0ff6e2e548ad72be9908253f8573cbf7766b
PotassiumChloride <kcl.nush@gmail.com>
2018-08-25 10:00:04 +0800

    jianxi3

--- app/src/main/java/com/kcl/nushhack/potassiumchloride/main.java
+++ app/src/main/java/com/kcl/nushhack/potassiumchloride/main.java
@@ -84,6 +84,9 @@ public class main extends AppCompatActivity
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
+        MenuItem mi = menu.findItem(R.id.signout);
+
+
         return true;
     }
 
--- app/src/main/java/com/kcl/nushhack/potassiumchloride/splashactivity.java
+++ app/src/main/java/com/kcl/nushhack/potassiumchloride/splashactivity.java
@@ -3,7 +3,15 @@ package com.kcl.nushhack.potassiumchloride;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 /**
  * Created by Kerk Tai Heng on 25/8/2018.
@@ -11,20 +19,57 @@ import android.support.v7.app.AppCompatActivity;
 
 public class splashactivity extends AppCompatActivity{
 
+    private FirebaseAuth mAuth;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        final Intent intent = new Intent(this, login.class);
 
+        /*
+        mAuth = FirebaseAuth.getInstance();
+
+        if(mAuth.getCurrentUser()==null){
+            startActivity(new Intent(this, login.class));
+            finish();
+        } else if(mAuth.getCurrentUser()!=null){
+            DatabaseReference ref = FirebaseDatabase.getInstance().getReference(main.USER_TABLE).child(mAuth.getCurrentUser().getUid());
+            ref.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    login.Current_user = dataSnapshot.getValue(user.class);
+                    startActivity(new Intent(splashactivity.this, main.class));
+                    finish();
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                }
+            });
+
+        } else {
+            new CountDownTimer(10000, 400) {
+
+                public void onTick(long millisUntilFinished) {
+                }
+
+                public void onFinish() {
+                    startActivity(new Intent(splashactivity.this, login.class));
+                    finish();
+                }
+            }.start();
+        }
+        */
         new CountDownTimer(800, 400) {
 
             public void onTick(long millisUntilFinished) {
             }
 
             public void onFinish() {
-                startActivity(intent);
+                startActivity(new Intent(splashactivity.this, login.class));
                 finish();
             }
         }.start();
+
 
 
     }
