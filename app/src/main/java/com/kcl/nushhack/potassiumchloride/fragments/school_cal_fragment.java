8dd03735e4882341ee4d24363842012d575cec09
andy5 <andy518420@gmail.com>
2018-08-25 09:18:22 +0800

    school cal

--- app/src/main/java/com/kcl/nushhack/potassiumchloride/fragments/school_cal_fragment.java
+++ app/src/main/java/com/kcl/nushhack/potassiumchloride/fragments/school_cal_fragment.java
@@ -1,14 +1,18 @@
 package com.kcl.nushhack.potassiumchloride.fragments;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Adapter;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.GridView;
 import android.widget.LinearLayout;
 import android.widget.TextClock;
@@ -58,45 +62,44 @@ public class school_cal_fragment extends Fragment {
         fragment.setArguments(args);
         return fragment;
     }
-    GridView grid;Text month_year;
+    GridView grid;TextView month_year;String[] content;int start;
     ArrayList<LinearLayout> days=new ArrayList<LinearLayout>();
     void loadCalendar(){
         //somehow get the firebase thing as a whole string
-        String info="";//pretend to be many commas seperated
-        String[] content=info.split(",");
-        grid=getActivity().findViewById(R.id.calendar_grid);
-        month_year=getActivity().findViewById(R.id.month_text);
-        month_year.setTextContent(content[0]+" "+content[1]);
-        int start=(Integer.parseInt(content[2])%7);
+        //month year day of 1st eventday1 eventday2 etc
+        String info="August,2018,2,No Event,No Event,No Event,No Event,No Event,No Event,No Event,No Event,No Event,No Event,No Event,No Event,No Event,No Event,No Event,No Event,No Event,No Event,No Event,No Event,No Event,No Event,No Event,Hackathon,Hackathon,No Event,No Event,No Event,No Event,No Event,No Event";//pretend to be many commas seperated
+        content=info.split(",");
+        month_year.setText(content[0]+"/"+content[1]);
+        start=((Integer.parseInt(content[2])+1)%7);
+        for (int i=0;i<start;i++){
+            LinearLayout ll=new LinearLayout(this.getContext());
+            ll.addView(new TextView(getContext()));
+            ll.addView(new TextView(getContext()));
+            days.add(ll);
+        }
         for(int i=3;i<content.length;i++){
             LinearLayout ll=new LinearLayout(this.getContext());
-            ll.setOrientation(LinearLayout.VERTICAL);
             TextView a=new TextView(this.getContext());
-            a.setTextSize(20);a.setText(i-2+"");
+            a.setTextSize(40);a.setText(i-2+"");
             ll.addView(a);
             TextView b=new TextView(this.getContext());
-            b.setTextSize(15);b.setText(content[3]);
+            b.setTextSize(5);b.setText(content[i]);
             ll.addView(b);
             days.add(ll);
-            i++;
         }
-        ArrayAdapter aa=new ArrayAdapter((getContext()),R.layout.fragment_school_cal_fragment,days.toArray());
-        grid.setAdapter(aa);
-
-    }
-    void initGrid(){
-
+        myAdapter ma=new myAdapter(getContext());
+        ma.days=content;
+        ma.start=start;
+        grid.setAdapter(ma);
     }
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        loadCalendar();
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_school_cal_fragment, container, false);
     }
 
@@ -138,4 +141,71 @@ public class school_cal_fragment extends Fragment {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
     }
+
+    TextView bigDate,bigEvent;
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        grid=getView().findViewById(R.id.calendar_grid);
+        month_year=getView().findViewById(R.id.month_text);
+        TextView x=getView().findViewById(R.id.days);
+        bigDate=getView().findViewById(R.id.bigDate);
+        bigEvent=getView().findViewById(R.id.bigEvent);
+        x.setText("S       M       T       W       T       F       S     ");
+        grid.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View v,
+                                    int position, long id) {
+                bigDate.setText(position-start+1);
+                bigEvent.setText(content[position-start+1+3]);
+            }
+        });
+        loadCalendar();
+    }
+}
+class myAdapter extends BaseAdapter{
+    Context mContext;
+    public myAdapter(Context c) {
+        mContext = c;
+    }
+    String[] days;
+    int start;
+    public int getCount() {
+        return days.length;
+    }
+
+    public Object getItem(int position) {
+        return null;
+    }
+
+    public long getItemId(int position) {
+        return 0;
+    }
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        //get the item corresponding to your position
+
+        LinearLayout row = (LinearLayout) (convertView == null
+                ? LayoutInflater.from(mContext).inflate(R.layout.day_item, parent, false)
+                : convertView);
+        try{
+            if(position<start)
+                throw new Exception();
+        ((TextView)row.findViewById(R.id.date)).setText(String.valueOf(position-start+1));
+        ((TextView)row.findViewById(R.id.msg)).setText(days[position-start+1+3]);
+        if(!days[position-start+1+3].equals("")&&!days[position-start+1+3].equals("No Event")){
+            row.setBackgroundColor(Color.CYAN);
+        }else{
+            row.setBackgroundColor(Color.BLUE);
+        }}
+        catch(Exception e){
+            ((TextView)row.findViewById(R.id.date)).setText("");
+            ((TextView)row.findViewById(R.id.msg)).setText("");
+
+        }
+        return row;
+    }
+
+
+
+
 }
