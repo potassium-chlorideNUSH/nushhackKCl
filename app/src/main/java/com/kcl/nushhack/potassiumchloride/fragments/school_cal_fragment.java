362332f5a4b636841511bb1e0af5db114481e2df
andy5 <andy518420@gmail.com>
2018-08-25 13:35:32 +0800

    school cal

--- app/src/main/java/com/kcl/nushhack/potassiumchloride/fragments/school_cal_fragment.java
+++ app/src/main/java/com/kcl/nushhack/potassiumchloride/fragments/school_cal_fragment.java
@@ -4,8 +4,10 @@ import android.content.Context;
 import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Debug;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,6 +19,7 @@ import android.widget.GridView;
 import android.widget.LinearLayout;
 import android.widget.TextClock;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.kcl.nushhack.potassiumchloride.R;
 
@@ -62,33 +65,40 @@ public class school_cal_fragment extends Fragment {
         fragment.setArguments(args);
         return fragment;
     }
-    GridView grid;TextView month_year;String[] content;int start;
+    static GridView grid;TextView month_year;String[] content;int start;
     ArrayList<LinearLayout> days=new ArrayList<LinearLayout>();
     void loadCalendar(){
         //somehow get the firebase thing as a whole string
         //month year day of 1st eventday1 eventday2 etc
-        String info="August,2018,2,No Event,No Event,No Event,No Event,No Event,No Event,No Event,No Event,No Event,No Event,No Event,No Event,No Event,No Event,No Event,No Event,No Event,No Event,No Event,No Event,No Event,No Event,No Event,Hackathon,Hackathon,No Event,No Event,No Event,No Event,No Event,No Event";//pretend to be many commas seperated
+        days.clear();
+        String info="August,2018,3,No Event,No Event,No Event,No Event,No Event,No Event,No Event,No Event,No Event,No Event,No Event,No Event,No Event,No Event,No Event,No Event,No Event,No Event,No Event,No Event,No Event,No Event,No Event,Hack,Hack,No Event,No Event,No Event,No Event,No Event,No Event";
         content=info.split(",");
-        month_year.setText(content[0]+"/"+content[1]);
-        start=((Integer.parseInt(content[2])+1)%7);
+        month_year.setText(content[0]+","+content[1]);
+        start=((Integer.parseInt(content[2]))%7);
         for (int i=0;i<start;i++){
             LinearLayout ll=new LinearLayout(this.getContext());
-            ll.addView(new TextView(getContext()));
-            ll.addView(new TextView(getContext()));
-            days.add(ll);
-        }
-        for(int i=3;i<content.length;i++){
-            LinearLayout ll=new LinearLayout(this.getContext());
-            TextView a=new TextView(this.getContext());
-            a.setTextSize(40);a.setText(i-2+"");
+            TextView a=new TextView(getContext());a.setText("");
+            TextView b=new TextView(getContext());a.setText("");
             ll.addView(a);
-            TextView b=new TextView(this.getContext());
-            b.setTextSize(5);b.setText(content[i]);
             ll.addView(b);
             days.add(ll);
         }
+        for(int i=start;i<content.length+start-3;i++){
+            LinearLayout ll=new LinearLayout(this.getContext());
+            TextView a=new TextView(this.getContext());
+            a.setTextSize(40);
+            ll.addView(a);
+            TextView b=new TextView(this.getContext());
+            b.setTextSize(5);
+            ll.addView(b);b.setText(content[i-start+3]);
+            days.add(ll);
+        }
         myAdapter ma=new myAdapter(getContext());
-        ma.days=content;
+        ma.days=new String[days.size()];
+        for(int i=0;i<days.size();i++){
+            String s=((TextView)(this.days.get(i).getChildAt(1))).getText().toString();
+            ma.days[i]=s;
+        }
         ma.start=start;
         grid.setAdapter(ma);
     }
@@ -142,7 +152,7 @@ public class school_cal_fragment extends Fragment {
         void onFragmentInteraction(Uri uri);
     }
 
-    TextView bigDate,bigEvent;
+    static TextView bigDate,bigEvent;
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         grid=getView().findViewById(R.id.calendar_grid);
@@ -155,8 +165,11 @@ public class school_cal_fragment extends Fragment {
             @Override
             public void onItemClick(AdapterView<?> parent, View v,
                                     int position, long id) {
-                bigDate.setText(position-start+1);
-                bigEvent.setText(content[position-start+1+3]);
+                try{
+                    if(position<start||position>start+days.size()-3)
+                        throw new Exception();
+                bigDate.setText(String.valueOf(position-start+1));
+                bigEvent.setText(String.valueOf(content[position-start+3]));}catch(Exception e){}
             }
         });
         loadCalendar();
@@ -183,25 +196,23 @@ class myAdapter extends BaseAdapter{
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         //get the item corresponding to your position
-
         LinearLayout row = (LinearLayout) (convertView == null
                 ? LayoutInflater.from(mContext).inflate(R.layout.day_item, parent, false)
                 : convertView);
-        try{
-            if(position<start)
-                throw new Exception();
+        if(position<start) {
+            ((TextView)row.findViewById(R.id.date)).setText("");
+            ((TextView)row.findViewById(R.id.msg)).setText("");
+            row.setBackgroundColor(Color.TRANSPARENT);
+        }else{
+            Log.d("ppppp",position+","+days[position-start+3]);
         ((TextView)row.findViewById(R.id.date)).setText(String.valueOf(position-start+1));
-        ((TextView)row.findViewById(R.id.msg)).setText(days[position-start+1+3]);
-        if(!days[position-start+1+3].equals("")&&!days[position-start+1+3].equals("No Event")){
+        ((TextView)row.findViewById(R.id.msg)).setText(days[position-start+3]);
+        if(!days[position-start+3].equals("")&&!days[position-start+3].equals("No Event")){
             row.setBackgroundColor(Color.CYAN);
         }else{
             row.setBackgroundColor(Color.BLUE);
         }}
-        catch(Exception e){
-            ((TextView)row.findViewById(R.id.date)).setText("");
-            ((TextView)row.findViewById(R.id.msg)).setText("");
 
-        }
         return row;
     }
 
